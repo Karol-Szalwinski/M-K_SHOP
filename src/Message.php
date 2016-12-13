3ef9fe054c06e3fa8ffeb0c24b4b28c0488a0907
Michal <borowieckimichal@o2.pl>
2016-12-13 23:41:07 +0100

    klasa wiadomosci

--- src/Message.php
+++ src/Message.php
@@ -1,18 +1,26 @@
 <?php
+/**
+ * klasa wiadomosci:
+ * - możliwość przesyłania wiadomosci do użytkownika
+ * - możliwość wyświetlania otrzymanych wiadomości u użytkownika
+ * - możliwość wyświetlania wysłanych wiadomości u administratora
+ * coś jeszcze ?
+ */
 
-//klasa wiadomości
 
 class Message {
 
     private $id;
-    private $textMessage;
     private $receiverId;
+    private $senderId;
+    private $textMessage;
     private $creationDate;
 
     public function __construct() {
         $this->id = -1;
-        $this->textMessage = "";
         $this->receiverId = 0;
+        $this->senderId = 0;
+        $this->textMessage = "";
         $this->creationDate = "";
     }
 
@@ -26,16 +34,6 @@ class Message {
         }
     }
 
-    public function setTextMessage($newTextMessage) {
-        if (is_string($newTextMessage)) {
-            $this->textMessage = $newTextMessage;
-        }
-    }
-
-    public function getTextMessage() {
-        return $this->textMessage;
-    }
-
     public function setReceiverId($newReceiverId) {
         if (is_numeric($newReceiverId)) {
             $this->receiverId = $newReceiverId;
@@ -46,6 +44,26 @@ class Message {
         return $this->receiverId;
     }
 
+    public function setSenderId($newSenderId) {
+        if (is_numeric($newSenderId)) {
+            $this->senderId = $newSenderId;
+        }
+    }
+
+    public function getSenderId() {
+        return $this->senderId;
+    }
+
+    public function setTextMessage($newTextMessage) {
+        if (is_string($newTextMessage)) {
+            $this->textMessage = $newTextMessage;
+        }
+    }
+
+    public function getTextMessage() {
+        return $this->textMessage;
+    }
+
     public function setCreationDate($newCreationDate) {
         if (is_string($newCreationDate)) {
             $this->creationDate = $newCreationDate;
@@ -54,6 +72,73 @@ class Message {
 
     public function getCreationDate() {
         return $this->creationDate;
+    }
+
+    public function saveToDB(mysqli $connection) {
+
+        if ($this->id == -1) {
+
+            //Saving new Message to DB
+
+            $sql = "INSERT INTO Messages(receiver_id, sender_id, text_message)
+                   VALUES ('$this->receiverId', '$this->senderId', '$this->textMessage')";
+
+            $result = $connection->query($sql);
+            if ($result == true) {
+                $this->id = $connection->insert_id;
+
+                return true;
+            } else {
+
+                return false;
+            }
+        }
+    }
+
+    static public function loadMessagesByReceiverId(mysqli $connection, $receiverId) {
+
+        $sql = "SELECT * FROM Messages WHERE receiver_id=$receiverId";
+
+        $result = $connection->query($sql);
+
+        if ($result == true && $result->num_rows == 1) {
+
+            $row = $result->fetch_assoc();
+            $loadedMessage = new Message();
+            $loadedMessage->id = $row['id'];
+            $loadedMessage->receiverId = $row['receiver_id'];
+            $loadedMessage->senderId = $row['sender_id'];
+            $loadedMessage->textMessage = $row['text_message'];
+            $loadedMessage->creationDate = $row['creation_date'];
+
+
+            return $loadedMessage;
+        }
+
+        return null;
+    }
+
+    static public function loadMessagesBySenderId(mysqli $connection, $senderId) {
+
+        $sql = "SELECT * FROM Messages WHERE sender_id=$senderId";
+
+        $result = $connection->query($sql);
+
+        if ($result == true && $result->num_rows == 1) {
+
+            $row = $result->fetch_assoc();
+            $loadedMessage = new Message();
+            $loadedMessage->id = $row['id'];
+            $loadedMessage->receiverId = $row['receiver_id'];
+            $loadedMessage->senderId = $row['sender_id'];
+            $loadedMessage->textMessage = $row['text_message'];
+            $loadedMessage->creationDate = $row['creation_date'];
+
+
+            return $loadedMessage;
+        }
+
+        return null;
     }
 
 }
