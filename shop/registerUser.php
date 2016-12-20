1b564cda1f7e8e478393671204329d966349aae9
Karol-Szalwinski <karol.szalwinski@gmail.com>
2016-12-20 11:32:16 +0100

    Poprawiłem formularz rejestracji - dodałem adres

--- shop/registerUser.php
+++ shop/registerUser.php
@@ -1,6 +1,5 @@
 <?php
-
-/* 
+/*
  * Strona rejestracji
  * Strona do rejestracji użytkownika. Ma pobierać wszystkie informacje o
  * użytkowniku.
@@ -31,13 +30,13 @@
                 <!—-----------Panel z kategoriami --------------->
                 <?php require_once __DIR__ . '/sidebar.php' ?>
 
-                <div class="col-sm-5 text-left"> 
+                <div class="col-sm-4 text-left"> 
 
                     <h3>Załóż konto</h3>
                     <form>
                         <div class="form-group">
                             <label for="email">Email:</label>
-                            <input type="text" class="form-control" id="email" placeholder="Podaj email">
+                            <input type="email" class="form-control" id="email" placeholder="Podaj email">
                         </div>
                         <div class="form-group">
                             <label for="name">Imię:</label>
@@ -45,7 +44,23 @@
                         </div>
                         <div class="form-group">
                             <label for="surname">Nazwisko:</label>
-                            <input type="email" class="form-control" id="surname" placeholder="Podaj nazwisko">
+                            <input type="text" class="form-control" id="surname" placeholder="Podaj nazwisko">
+                        </div>
+                        <div class="form-group">
+                            <label for="street">Ulica:</label>
+                            <input type="text" class="form-control" id="street" placeholder="Podaj ulicę">
+                        </div>
+                        <div class="form-group">
+                            <label for="no">Nr domu / lokalu:</label>
+                            <input type="text" class="form-control" id="no" placeholder="Podaj nr domu / lokalu">
+                        </div>
+                        <div class="form-group">
+                            <label for="postcode">Kod pocztowy:</label>
+                            <input type="text" class="form-control" id="postcode" placeholder="Podaj kod pocztowy">
+                        </div>
+                        <div class="form-group">
+                            <label for="city">Miejscowość:</label>
+                            <input type="text" class="form-control" id="city" placeholder="Podaj miejscowość">
                         </div>
                         <div class="form-group">
                             <label for="password1">Hasło:</label>
