a23e2a3c1ce4e9c96047ce274851e2d7f97739b4
Michal <borowieckimichal@o2.pl>
2017-01-03 21:12:40 +0100

    wyswietlanie zdjecia

--- admin/addProduct.php
+++ admin/addProduct.php
@@ -1,9 +1,5 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 require_once __DIR__ . '/../src/required.php';
 //jeśli user jest zalogowany to przekierowuję na główną
 if (!isLoggedAdmin($conn)) {
@@ -26,6 +22,7 @@ If ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['fileToUpload'])) {
             // Zapis do tablicy sesyjnej
             $_SESSION['photo'][] = [$uploadFile];
             var_dump($_SESSION['photo']);
+            echo '<img src="'.$uploadFile.'" width="200px" height="150px" />';
         }
     }
 }
