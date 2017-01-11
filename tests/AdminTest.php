0d197c1177a8fd786a31bc368fe57817d1462e75
Michal <borowieckimichal@o2.pl>
2017-01-11 22:28:42 +0100

    testy klasy Admin

--- tests/AdminTest.php
+++ tests/AdminTest.php
@@ -1,8 +1,18 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once _DIR_ . '/src/Admin.php';
 
+class AdminTest extends PHPUnit_Extensions_Database_TestCase {
+
+    public function getConnection() {
+        $conn = new PDO(
+                $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']
+        );
+        return new PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection($conn, $GLOBALS['DB_NAME']);
+    }
+
+    public function getDataSet() {
+        return $this->createFlatXmlDataSet(__DIR__ . '/datasets/Admin.xml');
+    }
+
+}
